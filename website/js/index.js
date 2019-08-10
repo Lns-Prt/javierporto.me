bd98d8173481b1f185a5fcaad64be83d4661e0ad
Lns-Prt <javirux@gmail.com>
2019-08-10 14:55:53 +0200

    Responsive design

--- website/js/index.js
+++ website/js/index.js
@@ -6,7 +6,7 @@ const sticky = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 const setNavbar = () => {
     const width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-    if (window.pageYOffset >= sticky && width > 600) {
+    if (window.pageYOffset >= sticky && width > 768) {
             navbar.classList.add("sticky", "translucent")
     } else {
         navbar.classList.remove("sticky", "translucent");
